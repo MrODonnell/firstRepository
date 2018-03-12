fa1b46f8f49f670785fea3425689f189c84289d1
Zach Gray <zacharygray187@gmail.com>
2018-03-12 11:52:52 -0400

    initialize pythoncheatsheet.py

---
+++ pythoncheatsheet.py
@@ -0,0 +1,166 @@
+#adding
+adding = 3 + 7
+print("The operator for addition is: +")
+print(adding)
+print('\n')
+#subtracting-
+#multiplying*
+#shoe**
+#dividing/
+#round//
+def print_lyrics():
+	print("I'm a lumberjack, and I'm okay.")
+	print("I sleep all night and I work all day.")
+	print+Lyrics()
+
+def return_lyrica():
+	lyrics="I'm a lumberjack, and I'm okay."
+	lyrics=lyrics + "I sleep all night and i work all day."
+
+	
+def somefunction():
+	print("this,that")
+	print("and something")
+
+somefunction()
+
+
+i=0
+while i<0:
+	print("hello")
+	i+=1
+	
+for number in range(12,20):
+	print(number)
+	
+
+i=0
+j=0
+while i<5:
+	while j<5:
+		print(j, end='*')
+		j= j + 1
+	print('\n################')
+	j = 0
+	i += 1
+
+# To get started you need to be aware of three data types:
+# int is an Integer
+# float is a decimal
+# sting is letters, words, whitespace, and special characters including numbers
+
+# Addition uses the + operator
+num1 = 3
+num2 = 4
+
+sum = (num1) + (num2)
+
+print('-The Sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# Subtraction uses the - operator
+num1 = 9
+num2 = 6
+
+sum = (num1) - (num2)
+
+print('-The Difference of {0} and {1} is {2}'.format(num1, num2, sum))
+# Multiplication uses the * operator
+num1 = 2
+num2 = 5
+
+sum = (num1) * (num2)
+
+print('-The Product of {0} and {1} is {2}'.format(num1, num2, sum)) 
+# exponent uses ** as the operator
+num1 = 7
+num2 = 3
+
+sum = (num1) ** (num2)
+
+print('-The Power of {0} and {1} is {2}'.format(num1, num2, sum))
+# Division is more tricky than you might think.
+# Be aware that in Python 3 division results in a float
+
+# Division uses / as the operator and returns a float
+num1 = 12
+num2 = 5
+
+sum = (num1) / (num2)
+
+print('-The Quotient of {0} and {1} is {2}'.format(num1, num2, sum))
+# Even if the division results in an integer division returns a float
+
+# With floor division the result is truncated, not rounded.
+
+# Modulo essentially the remainder
+num1 = 12
+num2 = 5
+
+sum = (num1) % (num2)
+
+print('-The Quotient of {0} and {1} is {2}'.format(num1, num2, sum))
+#### day two ####
+
+# a void function has no return value
+# but it may output, like using a print statement
+
+
+
+# The return statement will stop a function
+# and optionally pass an expression back to the caller
+ 
+#### Day Three ####
+
+# Scope
+
+# Passing an argument to a function
+
+# Boolean operators
+
+# equals
+
+# not equal 
+
+# greater than
+
+# greater than or equal to
+
+# less than
+
+# less than or equal to
+
+# if statement and elif
+
+# while loop using an increment
+
+# while loop using range()
+
+
+# nested loop
+
+
+# Day four
+
+# Lists
+
+# create a list
+
+# retrieve a value using an index number
+
+# lists are zero indexed, so the first element is 0
+
+# negative number count back from the last item
+
+# There is a shortcut to loop through a list
+
+# The keyword "for" loops through a list
+# The keyword "in" assigns each value to the variable that comes before "in"
+
+# List slice
+
+# Dictionaries
+
+# As directed by pep 8 do not exceed 80 characters per line
+
+# loop dictionary using items
+
+# The hanging indentation helps keep 80 char limit and improve readability
